91bb7c2b56bd5ec430daef574e5f31a1a521b84f
John Carlo Reyes <johncarloreyes018@gmail.com>
2024-10-03 14:05:30 +0800

    Add files via upload

---
+++ Exercise 6/contact.php
@@ -0,0 +1,87 @@
+<?php include 'includes/header.php'; ?>
+
+		<!-- banner -->
+		<div class="banner_w3lspvt-2">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="index.html" class="font-weight-bold">Home</a></li>
+				<li class="breadcrumb-item" aria-current="page">Contact Us</li>
+			</ol>
+		</div>
+		<!-- //banner -->
+	</div>
+	<!-- //main banner -->
+
+	<!-- contact -->
+	<div class="contact py-5" id="contact">
+		<div class="container pb-xl-5 pb-lg-3">
+			<div class="row">
+				<div class="col-lg-6">
+					<img src="images/b2.png" alt="image" class="img-fluid" />
+				</div>
+				<div class="col-lg-6 mt-lg-0 mt-5">
+					<!-- contact form grid -->
+					<div class="contact-top1">
+						<form action="#" method="post" class="contact-wthree-do">
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-6">
+										<label>
+											First name
+										</label>
+										<input class="form-control" type="text" placeholder="Juan" name="name" required="">
+									</div>
+									<div class="col-md-6 mt-md-0 mt-4">
+										<label>
+											Last name
+										</label>
+										<input class="form-control" type="text" placeholder="Dela Cruz" name="name" required="">
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-6">
+										<label>
+											Mobile
+										</label>
+										<input class="form-control" type="text" placeholder="(+63) 9xxxxxxxxx" name="mobile" required="">
+									</div>
+									<div class="col-md-6 mt-md-0 mt-4">
+										<label>
+											Email
+										</label>
+										<input class="form-control" type="email" placeholder="example@gmail.com" name="email" required="">
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="row">
+									<div class="col-md-12">
+										<label>
+											Your message
+										</label>
+										<textarea placeholder="Add your Description here" name="message" class="form-control"></textarea>
+									</div>
+								</div>
+							</div>
+							<div class="row mt-3">
+								<div class="col-md-12">
+									<button type="submit" class="btn btn-cont-w3 btn-block mt-4">Send</button>
+								</div>
+							</div>
+						</form>
+					</div>
+					<!-- //contact form grid ends here -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //contact-->
+
+	<!-- map -->
+	<div class="w3l-map p-4">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3864.6569465764564!2d121.02285307592392!3d14.389249482214584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d1047d11d4fb%3A0x672c55f2cba8880!2sPamantasan%20ng%20Lungsod%20ng%20Muntinlupa!5e0!3m2!1sen!2sph!4v1727908465883!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</div>
+	<!-- //map -->
+
+<?php include 'includes/footer.php'; ?>
